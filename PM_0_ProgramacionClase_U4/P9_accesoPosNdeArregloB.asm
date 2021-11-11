3037acb784b1a01efaff8655d8258fa6bcb212ed
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2021-11-11 09:04:29 -0600

    Clase 11 nov

--- PM_0_ProgramacionClase_U4/P9_accesoPosNdeArregloB.asm
+++ PM_0_ProgramacionClase_U4/P9_accesoPosNdeArregloB.asm
@@ -27,18 +27,20 @@ columnas_p9 dword 5
 		;Lógica del Programa
 		;cuando se trabaja con arreglos, los indices se recomienda sean 
 		;accedidos utilizando a los registros edi o esi. 
-
-		; para arreglos normales, es indistinta la utilización de un registro u otro
-
-		
+			
 		mov eax, 0  ; limpia del registro para no tener contenido basura
 
-		mov edi, 1 ; indice de las filas       ; fila 1   ; considerando que se comienza en 0
-		mov esi,  4 ; indice de las columnas    ; columna 1 ; considerando que se comienza en 0
+		;mov edi, 1 ; indice de las filas       ; fila 1 ; considerando que se comienza en 0
+		;mov esi,  4 ; loc de mem de la columna accesada ; columna 1 ; considerando que se comienza en 0
 
+
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+		ImpPantallaLn "Cual es el indice de la fila que deseas: "
+		call readint  ; eax
 
 		;ubicacion de la loc. de mem. asociada a la fila buscada = 4 * totalColumnas * indiceFila
-		mov eax, edi   ; mueve al indiceFila deseado a edi, para poder multiplicar
+		;mov eax, edi   ; mueve al indiceFila deseado a edi, para poder multiplicar
 		mov ebx, 4     ; total de bytes de cada elemento del arreglo (tamaño del tipo de dato)
 		mul ebx        ; multiplica por el total de bytes que tiene cada elemento en el arreglo 
 		mul columnas_p9 ; (4 * indiceFila) * totalColumnas
@@ -47,12 +49,26 @@ columnas_p9 dword 5
 					  ; de la fila buscada
 
 
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;						
+
+		ImpPantallaLn "Cual es el indice de la columna que deseas: "
+		call readint  ;eax
+		
+		mov ebx, 4
+		mul ebx
+
+		mov esi, eax ; posicionamiento en edi de la localidad de memoria asociada al indice
+					 ; de la columna buscada
+
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 		mov eax, arreglo_p9[edi + esi]
 
 		call writedec
 		call crlf
 		
-		
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 		exit	
